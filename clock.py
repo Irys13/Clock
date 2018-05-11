57f54d8a417d8a40fbfcf5638d50482ba7563e91
Evie <ewe727@gmail.com>
2018-05-11 21:06:41 +0100

    the app now makes notification sound every hour

--- clock.py
+++ clock.py
@@ -3,23 +3,25 @@
 from tkinter import *
 import tkinter as tk
 import time
+import playsound
 
 def display_time():
 	current_time = time.strftime("%H:%M:%S")
 	textyText.configure(text=current_time)
 	application.after(1000, display_time)
+	min = time.strftime('%M')
+	sec = time.strftime('%S')
+	if (int(min)==0 and int(sec)<4):
+		playsound.playsound('birds.mp3', True)	
 
 application = tk.Tk()
 application.title("Clock")
-application.geometry("300x100")
+application.geometry("300x50")
 application.configure(background="black")
 
 textyText = tk.Label(application, text="", font=("Helverica", 30), bg="black", fg="purple",)
 
-
 textyText.pack()
 display_time()
-B1 = tk.Button(application, text ="just a random button", relief=RAISED, cursor="heart")
-B1.pack()
 application.mainloop()
 
